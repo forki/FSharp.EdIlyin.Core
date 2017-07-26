c72025d6e586a66556e568aeca91d59fba5844b3
Ed Ilyin <ed.il@live.com>
2017-07-26 22:45:52 +0300

    -> 2.0.0-preview2-006497

--- build.fsx
+++ build.fsx
@@ -13,7 +13,7 @@ open System
 
 let buildDir  = "./build/"
 let appReferences = !! "/**/*.fsproj"
-let dotnetcliVersion = "1.0.4"
+let dotnetcliVersion = "2.0.0-preview2-006497"
 let mutable dotnetExePath = "dotnet"
 
 // --------------------------------------------------------------------------------------
