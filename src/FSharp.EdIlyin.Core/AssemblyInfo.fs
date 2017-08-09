b9178d488300dd7316ee3991d67b8917567b03fe
Ed Ilyin <ed.il@live.com>
2017-08-09 10:59:16 +0300

    Bump version to 1.0.17

--- src/FSharp.EdIlyin.Core/AssemblyInfo.fs
+++ src/FSharp.EdIlyin.Core/AssemblyInfo.fs
@@ -5,8 +5,8 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.EdIlyin.Core")>]
 [<assembly: AssemblyProductAttribute("FSharp.EdIlyin.Core")>]
 [<assembly: AssemblyDescriptionAttribute("Project has no summmary; update build.fsx")>]
-[<assembly: AssemblyVersionAttribute("1.0.16")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.16")>]
+[<assembly: AssemblyVersionAttribute("1.0.17")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.17")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 do ()
 
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.EdIlyin.Core"
     let [<Literal>] AssemblyProduct = "FSharp.EdIlyin.Core"
     let [<Literal>] AssemblyDescription = "Project has no summmary; update build.fsx"
-    let [<Literal>] AssemblyVersion = "1.0.16"
-    let [<Literal>] AssemblyFileVersion = "1.0.16"
+    let [<Literal>] AssemblyVersion = "1.0.17"
+    let [<Literal>] AssemblyFileVersion = "1.0.17"
     let [<Literal>] AssemblyConfiguration = "Release"
